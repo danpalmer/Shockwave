6fea90473585f6925b31a81733e2dc8ea9fe0cc7
Dan Palmer <dan.palmer@me.com>
2011-10-16 16:27:24 +0100

    updates

--- shockwave/api/views.py
+++ shockwave/api/views.py
@@ -4,10 +4,10 @@ from models import *
 
 def get_dataset_info(request, term):
 	term = term.replace(' ', ',')
-	dataset = Dataset.objects.get(name=term)
+	dataset = Dataset.objects.filter(name=term)
 	
 	if not dataset:
-		return HttpResponse('{"url":"%s", "stream":"true"}' % ("http://shockwave.dapl.me:8000/" + term))
+		return HttpResponse('{"url":"http://shockwave.dapl.me:8000/dataset/%s", "stream":"true"}' % term)
 	
 	return HttpResponse('{"url":"%s", "stream":"false"}' % ("http://odin.local/dataset/" + term))
 
